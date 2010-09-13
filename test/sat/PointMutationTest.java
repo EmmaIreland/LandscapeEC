f4382f06b802bf6f829383fdfec94d12234c26fe
mulli133 <mulli133@e5a9bd0e-96e4-4e5d-b9da-31dc9101860d>
2010-09-13 20:35:02 +0000

    Finished the point mutation test (for real this time).
    git-svn-id: http://cscirepo.morris.umn.edu/svn/research/LandscapeEC@3398 e5a9bd0e-96e4-4e5d-b9da-31dc9101860d

--- test/sat/PointMutationTest.java
+++ test/sat/PointMutationTest.java
@@ -1,35 +1,56 @@
 package sat;
 
-import static org.junit.Assert.*;
+import java.util.Map;
 
-import org.junit.Before;
 import org.junit.Test;
 
+import static junit.framework.Assert.*;
+
 import parameters.GlobalParameters;
+import sat.operators.PointMutation;
+import util.ChiSquaredStatistics;
 import util.FrequencyCounter;
 import util.MockParameters;
-
+import util.ProbabilityUtils;
 
 public class PointMutationTest {
-
-	private static final int NUM_BITS = 8;
-
-	@Before
-	public void setUp() {
-		MockParameters mockParams = new MockParameters();
-        mockParams.put("AVERAGE_MUTATIONS", "1");
+    private static final double AVERAGE_MUTATIONS = 1;
+    private static final int NUM_BITS = 9;
+    private static final int NUM_TESTS = 100;
+    
+    @Test
+    public void testPointMutation() {
+        MockParameters mockParams = new MockParameters();
+        mockParams.put("AVERAGE_MUTATIONS", ""+AVERAGE_MUTATIONS);
         GlobalParameters.setParameters(mockParams);
-	}
-	
-	@Test
-	public void testMutationDistribution() {
-		Individual testIndividual = IndividualFactory.getInstance(NUM_BITS);
-		FrequencyCounter<Integer> freqCounter = new FrequencyCounter<Integer>();
-		
-		for (int i = 0; i < 50; i++) {
-			
-		}
-		fail("Test Incomplete");
-	}
-	
+        
+        SatInstance satInstance = new SatInstance();
+        satInstance.setNumVariables(NUM_BITS);
+
+        FrequencyCounter<Integer> counter = new FrequencyCounter<Integer>();
+        
+        for(int m=0; m<NUM_TESTS; m++) {
+            Individual individual = new Individual(generateStringOfZeroes(NUM_BITS));
+            
+            PointMutation mutationOperator = new PointMutation();
+            individual = mutationOperator.mutate(individual);
+            
+            int bitsFlipped = 0;
+            for(int b=0; b<NUM_BITS; b++) {
+                if(individual.getBitString().charAt(b) == '1') bitsFlipped++;
+            }
+            counter.addItem(bitsFlipped);
+        }
+        
+        Map<Integer, Double> expectedDistribution = ProbabilityUtils.binomialDistribution(NUM_BITS, AVERAGE_MUTATIONS/NUM_BITS);
+        
+        assertTrue("Mutation distribution " + counter + " does not match expected distribution " + expectedDistribution + " with confidence.",
+                ChiSquaredStatistics.chiSquaredTest(counter, expectedDistribution));
+    }
+    
+    private String generateStringOfZeroes(int length) {
+        StringBuilder string = new StringBuilder();
+        while(string.length() < length) string.append("0");
+        return string.toString();
+    }
 }
