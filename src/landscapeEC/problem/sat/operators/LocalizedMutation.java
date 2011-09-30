231742d6b0481364dbaa4597caac27a5c7ec8a34
robin775 <robin775@e5a9bd0e-96e4-4e5d-b9da-31dc9101860d>
2011-09-30 20:16:38 +0000

    Functionally identical to PointMutation - Important for Casey's work though, so don't delete it.
    git-svn-id: http://cscirepo.morris.umn.edu/svn/research/LandscapeEC@7317 e5a9bd0e-96e4-4e5d-b9da-31dc9101860d

---
+++ src/landscapeEC/problem/sat/operators/LocalizedMutation.java
@@ -0,0 +1,29 @@
+package landscapeEC.problem.sat.operators;
+
+import landscapeEC.parameters.DoubleParameter;
+import landscapeEC.problem.sat.Individual;
+import landscapeEC.util.SharedPRNG;
+
+public class LocalizedMutation implements MutationOperator {
+
+    @Override
+    public Individual mutate(Individual ind) {
+        int[] bits = ind.getBits();
+
+        double mutationRate = DoubleParameter.AVERAGE_MUTATIONS.getValue()/bits.length;
+        
+        for (int i = 0; i < bits.length; i++) {
+            if (SharedPRNG.instance().nextDouble() < mutationRate) {
+                bits[i] = flipBit(bits[i]);
+            }
+        }
+
+        return new Individual(bits);
+    }
+
+    private int flipBit(int bit) {
+        if (bit == 0)
+            return 1;
+        return 0;
+    }
+}
