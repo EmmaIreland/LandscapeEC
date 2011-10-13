23fcf1625564942b4cfee3d9d15b210243b5322d
ellgr001 <ellgr001@e5a9bd0e-96e4-4e5d-b9da-31dc9101860d>
2011-10-13 20:27:17 +0000

    Further cleanup of SVN-related issues from previous revision. It should now be in a reasonable state.
    git-svn-id: http://cscirepo.morris.umn.edu/svn/research/LandscapeEC@7392 e5a9bd0e-96e4-4e5d-b9da-31dc9101860d

--- scenarios/locality/ShellScenario.java
+++ scenarios/locality/ShellScenario.java
@@ -1,26 +1,10 @@
 package locality;
 
-import java.util.List;
+import org.jbehave.scenario.Scenario;
 
-import org.jbehave.core.configuration.Configuration;
-import org.jbehave.core.configuration.MostUsefulConfiguration;
-import org.jbehave.core.io.LoadFromClasspath;
-import org.jbehave.core.junit.JUnitStory;
-import org.jbehave.core.reporters.Format;
-import org.jbehave.core.reporters.StoryReporterBuilder;
-import org.jbehave.core.steps.InstanceStepsFactory;
-import org.jbehave.core.steps.CandidateSteps;
+public class ShellScenario extends Scenario {
 
-public class ShellScenario extends JUnitStory {
-
-    @Override
-    public Configuration configuration(){
-	return new MostUsefulConfiguration().useStoryLoader(new LoadFromClasspath(this.getClass())).useStoryReporterBuilder(new StoryReporterBuilder().withDefaultFormats().withFormats(Format.CONSOLE, Format.TXT));
-    }
-    
-    @Override
-    public List<CandidateSteps> candidateSteps() {        
-        // varargs, can have more that one steps classes
-        return new InstanceStepsFactory(configuration(), new NeighborhoodSteps()).createCandidateSteps();
-    }
+	public ShellScenario() {
+		super(new NeighborhoodSteps());
+	}
 }
