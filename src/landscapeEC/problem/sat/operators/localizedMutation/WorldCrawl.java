c3e235abeb1c3695099ea1df0b7ab6278fbca134
robin775 <robin775@e5a9bd0e-96e4-4e5d-b9da-31dc9101860d>
2012-02-10 00:23:46 +0000

    WorldCrawl now actually works, where before it was not working. Repeat after me.  "Dot equals is bad.  Arrays dot equals is good."  Let us never forget this lesson.
    Added placeholding code for a new visualizer, which will show mutation rates and individual fitness as scaled colors blue and red, respectively.
    
    git-svn-id: http://cscirepo.morris.umn.edu/svn/research/LandscapeEC@8300 e5a9bd0e-96e4-4e5d-b9da-31dc9101860d

--- src/landscapeEC/problem/sat/operators/localizedMutation/WorldCrawl.java
+++ src/landscapeEC/problem/sat/operators/localizedMutation/WorldCrawl.java
@@ -1,6 +1,7 @@
 package landscapeEC.problem.sat.operators.localizedMutation;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Hashtable;
 import java.util.List;
@@ -40,61 +41,60 @@ public class WorldCrawl implements ConcentrationRanker{
     private ArrayList<Vector> directions = new ArrayList<Vector>();
     
     public WorldCrawl(){
-	Integer[] set = new Integer[2];
-	set[0]=-1;
-	set[1]=0;
-	left = new Vector(set);
-	set[0]=1;
-	right = new Vector(set);
-	set[0]=0;
-	set[1]=-1;
-	down=new Vector(set);
-	set[1]=1;
-	up=new Vector(set);
-	directions.add(up);
-	directions.add(down);
-	directions.add(left);
-	directions.add(right);
+    	Integer[] set = new Integer[2];
+    	set[0]=-1;
+    	set[1]=0;
+    	left = new Vector(set);
+    	set[0]=1;
+    	right = new Vector(set);
+    	set[0]=0;
+    	set[1]=-1;
+    	down=new Vector(set);
+    	set[1]=1;
+    	up=new Vector(set);
+    	directions.add(up);
+    	directions.add(down);
+    	directions.add(left);
+    	directions.add(right);
     }
     
     @Override
     public int getAmp(Vector vector) {
-	if(speciesConcentrationMap.containsKey(vector))
-        return speciesConcentrationMap.get(vector);
-	System.out.println("Something bad happened!");
-	return 0;
+    	if(speciesConcentrationMap.containsKey(vector))
+    		return speciesConcentrationMap.get(vector);
+    	System.out.println("getAmp called for "+vector+" - Found no entry in concentrationMap.");
+    	return 1;
     }
-
+    
     @Override
     public void initialize(GridWorld newWorld, int generationNumber){
-	world = newWorld;
+    	world = newWorld;
         Vector current;
-	setupUnprocessed();
+        setupUnprocessed();
         List<Vector> currentGroup;
         while(!unprocessed.isEmpty()){
-            //Having -1s in here is icky, yes.
-            //But due to the nature of ArrayList deletion,
-            //removing the first item of an ArrayList repeatedly
-            //involves shifting every subsequent item to the left
-            //so it would invoke a great deal of computational
-            //overhead.
-            //Better ideas welcome.
             current = unprocessed.remove(unprocessed.size()-1);
-            currentGroup = startCrawl(current);
+            currentGroup = crawl(current);
             if(currentGroup.size()>maxGroupSize){
                 maxGroupSize=currentGroup.size();
-                maxGroupSpecies=findBestInCell(currentGroup.get(0));
+                maxGroupSpecies=findBestInCell(current);
             }
             for(Vector v : currentGroup){
-        	speciesConcentrationMap.put(v, currentGroup.size());
+            	speciesConcentrationMap.put(v, currentGroup.size());
             }
             
+        }
+        if(generationNumber == 200){
+        	System.out.println("Generation 200!  Have a map.");
+        	System.out.println(speciesConcentrationMap);
+        }
+        if(maxGroupSize > 1){
+        	System.out.println("Biggest group size: "+maxGroupSize);
         }
         
     }
     private void setupUnprocessed() {
 	unprocessed.clear();
-        Vector current;
         for(Location<Vector> l : world){
             if(!l.getIndividuals().isEmpty()){
         	unprocessed.add(l.getPosition());
@@ -102,44 +102,19 @@ public class WorldCrawl implements ConcentrationRanker{
         }
     }
     
-    //We start through startCrawl rather than going right to continueCrawl logic
-    //because we have some information about the _first_ block we select which
-    //we will not have about others - because we are selecting the last element
-    //in unprocessed with which to start the crawl, we know that it is the upper
-    //-rightmost (9,9) element which has not yet been crawled.
-    //So for this first 'crawl', we do not need to check the upward or rightward
-    //directions.  For subsequent elements, this is untrue.
-    private List<Vector> startCrawl(Vector current){
-	List<Vector> result = new ArrayList<Vector>();
-	result.add(current);
-	int[] species = findBestInCell(current);
-	Vector next = current.plus(down);
-        if(unprocessed.contains(next) && species.equals(findBestInCell(next))){
-            unprocessed.remove(next);
-            result.addAll(continueCrawl(next, species));
-        }
-        next = current.plus(left);
-        if(unprocessed.contains(next)&& species.equals(findBestInCell(next))){
-            unprocessed.remove(next);
-            result.addAll(continueCrawl(next, species));
-        }
-	return result;
-    }
-    
-    private List<Vector> continueCrawl(Vector current, int[] species){
-	List<Vector> result = new ArrayList<Vector>();
-	result.add(current);
-	Vector next;
-	for(Vector v : directions){
-	    next = current.plus(v);
-	    if(unprocessed.contains(next)){
-		if(findBestInCell(next).equals(species)){
-                    unprocessed.remove(next);
-		    result.addAll(continueCrawl(next, species));
+    private List<Vector> crawl(Vector current){
+    	List<Vector> result = new ArrayList<Vector>();
+		result.add(current);
+		int[] species = findBestInCell(current);
+		Vector next;
+		for(Vector v : directions){
+	    	next = current.plus(v);
+	    	if(unprocessed.contains(next) && Arrays.equals(findBestInCell(next), species)){
+	   			unprocessed.remove(next);
+	   			result.addAll(crawl(next));
+    		}
 		}
-	    }
-	}
-	return result;
+		return result;
     }
     
     private int[] findBestInCell(Vector position) {
