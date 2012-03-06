285915c9c0bfe194bcccafd121474c1110672a46
cornh044 <cornh044@e5a9bd0e-96e4-4e5d-b9da-31dc9101860d>
2012-03-06 20:55:59 +0000

    Made progress on 3DWorld generation.
    git-svn-id: http://cscirepo.morris.umn.edu/svn/research/LandscapeEC@8802 e5a9bd0e-96e4-4e5d-b9da-31dc9101860d

--- src/landscapeEC/graphWorldGenerator/ThreeDWorldGenerator.java
+++ src/landscapeEC/graphWorldGenerator/ThreeDWorldGenerator.java
@@ -1,16 +1,19 @@
 package landscapeEC.graphWorldGenerator;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class ThreeDWorldGenerator {
 	
 	private int WorldSize;
+	private int WorldSizeSq;
 	private List<List<Integer>> listOfNeighborhoods;
 	
 	public File generate3DWorld (int worldSize) {
 		WorldSize = worldSize;
+		WorldSizeSq = (int) java.lang.Math.pow(WorldSize, 2);
 		for (int i = 0; i >= java.lang.Math.pow(WorldSize, 3); i++) {
 			//add and check top layer
 			//add and check middle layer
@@ -21,26 +24,70 @@ public class ThreeDWorldGenerator {
 		return null;
 	}
 	
-	private boolean checkLocation (int location) {
-		boolean validity = true;
+	private List<Integer> addLocations (int location) {
+		List<Integer> loc = new ArrayList<Integer>();
+		//add all spots to list
+		//left and right nodes
+		loc.add(location+1);
+		loc.add(location-1);
+		//lower and lower left and right nodes
+		loc.add(location+WorldSize);
+		loc.add(location+WorldSize+1);
+		loc.add(location+WorldSize-1);
+		//upper and upper left and right nodes
+		loc.add(location-WorldSize);
+		loc.add(location-WorldSize+1);
+		loc.add(location-WorldSize-1);
+		
+		//above layer
+		//left and right nodes
+		loc.add(location-WorldSizeSq);
+		loc.add(location+1-WorldSizeSq);
+		loc.add(location-1-WorldSizeSq);
+		//lower and lower left and right nodes
+		loc.add(location+WorldSize-WorldSizeSq);
+		loc.add(location+WorldSize+1-WorldSizeSq);
+		loc.add(location+WorldSize-1-WorldSizeSq);
+		//upper and upper left and right nodes
+		loc.add(location-WorldSize-WorldSizeSq);
+		loc.add(location-WorldSize+1-WorldSizeSq);
+		loc.add(location-WorldSize-1-WorldSizeSq);
+		
+		//below layer
+		//left and right nodes
+		loc.add(location+WorldSizeSq);
+		loc.add(location+1+WorldSizeSq);
+		loc.add(location-1+WorldSizeSq);
+		//lower and lower left and right nodes
+		loc.add(location+WorldSize+WorldSizeSq);
+		loc.add(location+WorldSize+1+WorldSizeSq);
+		loc.add(location+WorldSize-1+WorldSizeSq);
+		//upper and upper left and right nodes
+		loc.add(location-WorldSize+WorldSizeSq);
+		loc.add(location-WorldSize+1+WorldSizeSq);
+		loc.add(location-WorldSize-1+WorldSizeSq);
+		
+		//END adding locations
+		
+		
+		
 		//checks if number is on outer
 		if ((location % (java.lang.Math.pow(WorldSize, 3))) <= WorldSize) {
-			validity = false;
+			
 		}
 		//checks if number is on inner edge
 		if ((location+5 % (java.lang.Math.pow(WorldSize, 3))) <= WorldSize) {
-			validity = false;
 		}
 		//checks if number is on left edge
 		if ((location-1 % WorldSize) == 0) {
-			validity = false;
 		}
 		if ((location % WorldSize) == 0) {
-			validity = false;
 		}
 		
-		return validity;
+		return null;
 	}
+	
+
 	
 }
 
