559f0d0b91cd912971408f3a53b6cbe90d1aadd6
cornh044 <cornh044@e5a9bd0e-96e4-4e5d-b9da-31dc9101860d>
2012-03-07 00:49:52 +0000

    Got GraphWorld Generator to generate cubes.
    git-svn-id: http://cscirepo.morris.umn.edu/svn/research/LandscapeEC@8807 e5a9bd0e-96e4-4e5d-b9da-31dc9101860d

--- src/landscapeEC/graphWorldGenerator/ThreeDWorldGenerator.java
+++ src/landscapeEC/graphWorldGenerator/ThreeDWorldGenerator.java
@@ -10,8 +10,8 @@ import java.io.FileWriter;
 
 public class ThreeDWorldGenerator {
 
-	private int WorldSize;
-	private int WorldSizeSq;
+	private int worldSize;
+	private int worldSizeSq;
 	private LinkedList<LinkedList<Integer>> listOfNeighborhoods;
 
 	public static void main(String[] args) throws Exception{
@@ -20,17 +20,18 @@ public class ThreeDWorldGenerator {
 	}
 
 	private ThreeDWorldGenerator(int worldSize) {
-		WorldSize = worldSize;
-		WorldSizeSq = (int) java.lang.Math.pow(WorldSize, 2);
+		this.worldSize = worldSize;
+		worldSizeSq = (int) java.lang.Math.pow(worldSize, 2);
 	}
 
 	private void generate3DWorld () {
 		listOfNeighborhoods = new LinkedList<LinkedList<Integer>>();
-		for (int i = 0; i <= java.lang.Math.pow(WorldSize, 3); i++) {
+
+		for (int i = 0; i < java.lang.Math.pow(worldSize, 3); i++) {
 			listOfNeighborhoods.add(this.addLocations(i));
 		}
 
-		String name = "graphWorldFiles/3DWorldSize" + WorldSize;
+		String name = "graphWorldFiles/3DWorldSize" + worldSize;
 		this.writeYAMLFile(name);
 
 	}
@@ -43,7 +44,7 @@ public class ThreeDWorldGenerator {
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
-		for (int i = 0; i <= java.lang.Math.pow(WorldSize, 3); i++) {
+		for (int i = 0; i < java.lang.Math.pow(worldSize, 3); i++) {
 			String header = "--- # " + i + "\n";
 			try {
 				output.write(header);
@@ -70,86 +71,101 @@ public class ThreeDWorldGenerator {
 	}
 
 	private LinkedList<Integer> addLocations (int location) {
-		LinkedList<Integer> loc = this.addAllLocations(location);
-		return this.removeInvalidLocations(location, loc);
+		LinkedList<Integer> loc = new LinkedList<Integer>();
+ 		int x = location % worldSize;
+		int y = (location/worldSize) % worldSize;
+		int z = (location/worldSizeSq) % worldSize;
+		
+		for (int i = Math.max(x-1, 0); i <= Math.min(x+1, worldSize - 1); ++i){
+			for (int j = Math.max(y-1, 0); j <= Math.min(y+1, worldSize - 1); ++j){
+				for (int k = Math.max(z-1, 0); k <= Math.min(z+1, worldSize - 1); ++k){
+					loc.add(i+worldSize*j+worldSizeSq*k);
+				}
+			}
+		}
+		loc.removeFirstOccurrence(location);
+		return loc;
+
+		//		LinkedList<Integer> loc = this.addAllLocations(location);
+		//		return this.removeInvalidLocations(location, loc);
 	}
 
 	private LinkedList<Integer> removeInvalidLocations(int location,	LinkedList<Integer> locations) {
 		LinkedList<Integer> loc = locations;
 		//checks if number is on outer edge
-		if ((location+1 % (java.lang.Math.pow(WorldSize, 3))) <= WorldSize) {
-			loc.removeFirstOccurrence(location-WorldSize);
-			loc.removeFirstOccurrence(location-WorldSize-1);
-			loc.removeFirstOccurrence(location-WorldSize+1);
-			loc.removeFirstOccurrence(location-WorldSize-WorldSizeSq);
-			loc.removeFirstOccurrence(location-WorldSize-1-WorldSizeSq);
-			loc.removeFirstOccurrence(location-WorldSize+1-WorldSizeSq);
-			loc.removeFirstOccurrence(location-WorldSize+WorldSizeSq);
-			loc.removeFirstOccurrence(location-WorldSize-1+WorldSizeSq);
-			loc.removeFirstOccurrence(location-WorldSize+1+WorldSizeSq);
+		if ((location+1 % (java.lang.Math.pow(worldSize, 3))) <= worldSize) {
+			loc.removeFirstOccurrence(location-worldSize);
+			loc.removeFirstOccurrence(location-worldSize-1);
+			loc.removeFirstOccurrence(location-worldSize+1);
+			loc.removeFirstOccurrence(location-worldSize-worldSizeSq);
+			loc.removeFirstOccurrence(location-worldSize-1-worldSizeSq);
+			loc.removeFirstOccurrence(location-worldSize+1-worldSizeSq);
+			loc.removeFirstOccurrence(location-worldSize+worldSizeSq);
+			loc.removeFirstOccurrence(location-worldSize-1+worldSizeSq);
+			loc.removeFirstOccurrence(location-worldSize+1+worldSizeSq);
 
 		}
 
 		//checks if number is on inner edge
-		if ((location+5+1 % (java.lang.Math.pow(WorldSize, 3))) <= WorldSize) {
-			loc.removeFirstOccurrence(location+WorldSize);
-			loc.removeFirstOccurrence(location+WorldSize-1);
-			loc.removeFirstOccurrence(location+WorldSize+1);
-			loc.removeFirstOccurrence(location+WorldSize-WorldSizeSq);
-			loc.removeFirstOccurrence(location+WorldSize-1-WorldSizeSq);
-			loc.removeFirstOccurrence(location+WorldSize+1-WorldSizeSq);
-			loc.removeFirstOccurrence(location+WorldSize+WorldSizeSq);
-			loc.removeFirstOccurrence(location+WorldSize-1+WorldSizeSq);
-			loc.removeFirstOccurrence(location+WorldSize+1+WorldSizeSq);
+		if ((location+5+1 % (java.lang.Math.pow(worldSize, 3))) <= worldSize) {
+			loc.removeFirstOccurrence(location+worldSize);
+			loc.removeFirstOccurrence(location+worldSize-1);
+			loc.removeFirstOccurrence(location+worldSize+1);
+			loc.removeFirstOccurrence(location+worldSize-worldSizeSq);
+			loc.removeFirstOccurrence(location+worldSize-1-worldSizeSq);
+			loc.removeFirstOccurrence(location+worldSize+1-worldSizeSq);
+			loc.removeFirstOccurrence(location+worldSize+worldSizeSq);
+			loc.removeFirstOccurrence(location+worldSize-1+worldSizeSq);
+			loc.removeFirstOccurrence(location+worldSize+1+worldSizeSq);
 		}
 
 		//checks if number is on left edge
-		if ((location % WorldSize) == 0) {
+		if ((location % worldSize) == 0) {
 			loc.removeFirstOccurrence(location-1);
-			loc.removeFirstOccurrence(loc.add(location+WorldSize-1));
-			loc.removeFirstOccurrence(loc.add(location-WorldSize-1));
-			loc.removeFirstOccurrence(location-1-WorldSizeSq);
-			loc.removeFirstOccurrence(location+WorldSize-1-WorldSizeSq);
-			loc.removeFirstOccurrence(location-WorldSize-1-WorldSizeSq);
-			loc.removeFirstOccurrence(location-1+WorldSizeSq);
-			loc.removeFirstOccurrence(location+WorldSize-1+WorldSizeSq);
-			loc.removeFirstOccurrence(location-WorldSize-1+WorldSizeSq);
+			loc.removeFirstOccurrence(loc.add(location+worldSize-1));
+			loc.removeFirstOccurrence(loc.add(location-worldSize-1));
+			loc.removeFirstOccurrence(location-1-worldSizeSq);
+			loc.removeFirstOccurrence(location+worldSize-1-worldSizeSq);
+			loc.removeFirstOccurrence(location-worldSize-1-worldSizeSq);
+			loc.removeFirstOccurrence(location-1+worldSizeSq);
+			loc.removeFirstOccurrence(location+worldSize-1+worldSizeSq);
+			loc.removeFirstOccurrence(location-worldSize-1+worldSizeSq);
 		}
 		//checks if number is on right edge
-		if ((location + 1 % WorldSize) == 0) {
+		if ((location + 1 % worldSize) == 0) {
 			loc.removeFirstOccurrence(location+1);
-			loc.removeFirstOccurrence(location+WorldSize+1);
-			loc.removeFirstOccurrence(location-WorldSize+1);
-			loc.removeFirstOccurrence(location+1-WorldSizeSq);
-			loc.removeFirstOccurrence(location+WorldSize-1-WorldSizeSq);
-			loc.removeFirstOccurrence(location-WorldSize-1-WorldSizeSq);
-			loc.removeFirstOccurrence(location-1+WorldSizeSq);
-			loc.removeFirstOccurrence(location+WorldSize-1+WorldSizeSq);
-			loc.removeFirstOccurrence(location-WorldSize-1+WorldSizeSq);
+			loc.removeFirstOccurrence(location+worldSize+1);
+			loc.removeFirstOccurrence(location-worldSize+1);
+			loc.removeFirstOccurrence(location+1-worldSizeSq);
+			loc.removeFirstOccurrence(location+worldSize-1-worldSizeSq);
+			loc.removeFirstOccurrence(location-worldSize-1-worldSizeSq);
+			loc.removeFirstOccurrence(location-1+worldSizeSq);
+			loc.removeFirstOccurrence(location+worldSize-1+worldSizeSq);
+			loc.removeFirstOccurrence(location-worldSize-1+worldSizeSq);
 		}
 		//checks if something is on the top
-		if (location <= WorldSizeSq-1) {
-			loc.removeFirstOccurrence(location-WorldSizeSq);
-			loc.removeFirstOccurrence(location+1-WorldSizeSq);
-			loc.removeFirstOccurrence(location-1-WorldSizeSq);
-			loc.removeFirstOccurrence(location+WorldSize-WorldSizeSq);
-			loc.removeFirstOccurrence(location+WorldSize+1-WorldSizeSq);
-			loc.removeFirstOccurrence(location+WorldSize-1-WorldSizeSq);
-			loc.removeFirstOccurrence(location-WorldSize-WorldSizeSq);
-			loc.removeFirstOccurrence(location-WorldSize+1-WorldSizeSq);
-			loc.removeFirstOccurrence(location-WorldSize-1-WorldSizeSq);
+		if (location <= worldSizeSq-1) {
+			loc.removeFirstOccurrence(location-worldSizeSq);
+			loc.removeFirstOccurrence(location+1-worldSizeSq);
+			loc.removeFirstOccurrence(location-1-worldSizeSq);
+			loc.removeFirstOccurrence(location+worldSize-worldSizeSq);
+			loc.removeFirstOccurrence(location+worldSize+1-worldSizeSq);
+			loc.removeFirstOccurrence(location+worldSize-1-worldSizeSq);
+			loc.removeFirstOccurrence(location-worldSize-worldSizeSq);
+			loc.removeFirstOccurrence(location-worldSize+1-worldSizeSq);
+			loc.removeFirstOccurrence(location-worldSize-1-worldSizeSq);
 		}
 
-		if (location + 1 >= (java.lang.Math.pow(WorldSize, 3) - WorldSize)) {
-			loc.removeFirstOccurrence(location+WorldSizeSq);
-			loc.removeFirstOccurrence(location+1+WorldSizeSq);
-			loc.removeFirstOccurrence(location-1+WorldSizeSq);
-			loc.removeFirstOccurrence(location+WorldSize+WorldSizeSq);
-			loc.removeFirstOccurrence(location+WorldSize+1+WorldSizeSq);
-			loc.removeFirstOccurrence(location+WorldSize-1+WorldSizeSq);
-			loc.removeFirstOccurrence(location-WorldSize+WorldSizeSq);
-			loc.removeFirstOccurrence(location-WorldSize+1+WorldSizeSq);
-			loc.removeFirstOccurrence(location-WorldSize-1+WorldSizeSq);
+		if (location + 1 >= (java.lang.Math.pow(worldSize, 3) - worldSize)) {
+			loc.removeFirstOccurrence(location+worldSizeSq);
+			loc.removeFirstOccurrence(location+1+worldSizeSq);
+			loc.removeFirstOccurrence(location-1+worldSizeSq);
+			loc.removeFirstOccurrence(location+worldSize+worldSizeSq);
+			loc.removeFirstOccurrence(location+worldSize+1+worldSizeSq);
+			loc.removeFirstOccurrence(location+worldSize-1+worldSizeSq);
+			loc.removeFirstOccurrence(location-worldSize+worldSizeSq);
+			loc.removeFirstOccurrence(location-worldSize+1+worldSizeSq);
+			loc.removeFirstOccurrence(location-worldSize-1+worldSizeSq);
 		}
 
 		return loc;
@@ -161,41 +177,41 @@ public class ThreeDWorldGenerator {
 		loc.add(location+1);
 		loc.add(location-1);
 		//lower and lower left and right nodes
-		loc.add(location+WorldSize);
-		loc.add(location+WorldSize+1);
-		loc.add(location+WorldSize-1);
+		loc.add(location+worldSize);
+		loc.add(location+worldSize+1);
+		loc.add(location+worldSize-1);
 		//upper and upper left and right nodes
-		loc.add(location-WorldSize);
-		loc.add(location-WorldSize+1);
-		loc.add(location-WorldSize-1);
+		loc.add(location-worldSize);
+		loc.add(location-worldSize+1);
+		loc.add(location-worldSize-1);
 
 		//above layer
 		//left and right nodes
-		loc.add(location-WorldSizeSq);
-		loc.add(location+1-WorldSizeSq);
-		loc.add(location-1-WorldSizeSq);
+		loc.add(location-worldSizeSq);
+		loc.add(location+1-worldSizeSq);
+		loc.add(location-1-worldSizeSq);
 		//lower and lower left and right nodes
-		loc.add(location+WorldSize-WorldSizeSq);
-		loc.add(location+WorldSize+1-WorldSizeSq);
-		loc.add(location+WorldSize-1-WorldSizeSq);
+		loc.add(location+worldSize-worldSizeSq);
+		loc.add(location+worldSize+1-worldSizeSq);
+		loc.add(location+worldSize-1-worldSizeSq);
 		//upper and upper left and right nodes
-		loc.add(location-WorldSize-WorldSizeSq);
-		loc.add(location-WorldSize+1-WorldSizeSq);
-		loc.add(location-WorldSize-1-WorldSizeSq);
+		loc.add(location-worldSize-worldSizeSq);
+		loc.add(location-worldSize+1-worldSizeSq);
+		loc.add(location-worldSize-1-worldSizeSq);
 
 		//below layer
 		//left and right nodes
-		loc.add(location+WorldSizeSq);
-		loc.add(location+1+WorldSizeSq);
-		loc.add(location-1+WorldSizeSq);
+		loc.add(location+worldSizeSq);
+		loc.add(location+1+worldSizeSq);
+		loc.add(location-1+worldSizeSq);
 		//lower and lower left and right nodes
-		loc.add(location+WorldSize+WorldSizeSq);
-		loc.add(location+WorldSize+1+WorldSizeSq);
-		loc.add(location+WorldSize-1+WorldSizeSq);
+		loc.add(location+worldSize+worldSizeSq);
+		loc.add(location+worldSize+1+worldSizeSq);
+		loc.add(location+worldSize-1+worldSizeSq);
 		//upper and upper left and right nodes
-		loc.add(location-WorldSize+WorldSizeSq);
-		loc.add(location-WorldSize+1+WorldSizeSq);
-		loc.add(location-WorldSize-1+WorldSizeSq);
+		loc.add(location-worldSize+worldSizeSq);
+		loc.add(location-worldSize+1+worldSizeSq);
+		loc.add(location-worldSize-1+worldSizeSq);
 
 		return loc;
 	}
