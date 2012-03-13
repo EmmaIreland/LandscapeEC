97709680ec08fcb6101f5635e19d071609b6cfdd
robin775 <robin775@e5a9bd0e-96e4-4e5d-b9da-31dc9101860d>
2012-03-13 17:49:43 +0000

    Hey, look!  A test!
    git-svn-id: http://cscirepo.morris.umn.edu/svn/research/LandscapeEC@8872 e5a9bd0e-96e4-4e5d-b9da-31dc9101860d

---
+++ test/sat/OnePointCrossoverTest.java
@@ -0,0 +1,66 @@
+package sat;
+
+import static junit.framework.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import landscapeEC.problem.GlobalProblem;
+import landscapeEC.problem.Individual;
+import landscapeEC.problem.sat.SatInstance;
+import landscapeEC.problem.sat.operators.OnePointCrossover;
+import landscapeEC.util.FrequencyCounter;
+
+import org.junit.Test;
+
+import util.ChiSquaredStatistics;
+
+public class OnePointCrossoverTest {
+    private static final int NUM_BITS = 9;
+    private static final int NUM_TESTS = 100;
+    
+    @Test
+    public void testUniformCrossover() {        
+        SatInstance satInstance = new SatInstance(1.0);
+        satInstance.setNumVariables(NUM_BITS);
+        GlobalProblem.setProblem(satInstance);
+
+        FrequencyCounter<Integer> counter = new FrequencyCounter<Integer>();
+        
+        for(int m=0; m<NUM_TESTS; m++) {
+            List<Individual> parents = new ArrayList<Individual>();
+            parents.add(new Individual(generateStringOfZeroes(NUM_BITS), false));
+            parents.add(new Individual(generateStringOfOnes(NUM_BITS), false));
+            
+            OnePointCrossover crossoverOperator = new OnePointCrossover();
+            Individual child = crossoverOperator.crossover(parents);
+            
+            int i=child.getBit(0);
+            int checker = i;
+            while(child.getBit(i)==checker){
+            	i++;
+            }
+            assertTrue((checker == 1 && child.getBit(i) == 0) || (checker == 0) && child.getBit(i) == 1);
+            checker=child.getBit(i);
+            while(i<NUM_BITS){
+            	assertTrue(checker==child.getBit(i));
+            	i++;
+            }
+        }
+    }
+    
+    private String generateStringOfZeroes(int length) {
+        StringBuilder string = new StringBuilder();
+        while(string.length() < length) string.append("0");
+        return string.toString();
+    }
+    
+    private String generateStringOfOnes(int length) {
+        StringBuilder string = new StringBuilder();
+        while(string.length() < length) string.append("1");
+        return string.toString();
+    }
+}
+
