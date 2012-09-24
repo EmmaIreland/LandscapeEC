234ccbd41dff05c4063f39cf19c36b34d22d55ab
irela065 <irela065@e5a9bd0e-96e4-4e5d-b9da-31dc9101860d>
2012-09-24 21:52:26 +0000

    Got rid of findMinHammingDistance because it wasn't being used.
    git-svn-id: http://cscirepo.morris.umn.edu/svn/research/LandscapeEC@10560 e5a9bd0e-96e4-4e5d-b9da-31dc9101860d

--- src/landscapeEC/problem/ecc/EccEvaluator.java
+++ src/landscapeEC/problem/ecc/EccEvaluator.java
@@ -15,16 +15,15 @@ public class EccEvaluator extends Evaluator {
     
     @Override
     protected double doEvaluation(Problem problem, Individual individual) {
-        int numOfBitsPerWord = ((EccProblem) problem).getNumOfBitsPerWord();
-        int numOfCodeWords = ((EccProblem) problem).getNumOfCodeWords();
-        int[] individualString = individual.getBits();
+        numOfBitsPerWord = ((EccProblem) problem).getNumOfBitsPerWord();
+        numOfCodeWords = ((EccProblem) problem).getNumOfCodeWords();
+        individualString = individual.getBits();
         ArrayList<int[]> listOfCodeWords = new ArrayList<int[]>();
         
         for(int i=0; i<numOfCodeWords; i++){
-            listOfCodeWords.set(i, Arrays.copyOfRange(individualString, numOfBitsPerWord*i, numOfBitsPerWord*(i+1)-1));
+            listOfCodeWords.add(Arrays.copyOfRange(individualString, numOfBitsPerWord*i, numOfBitsPerWord*(i+1)));
         }
         
-        int minHamming = findMinHammingDistance(listOfCodeWords);
         
         double sumOfHamm = 0;
         
@@ -39,24 +38,7 @@ public class EccEvaluator extends Evaluator {
         
         
         return 1/sumOfHamm;
-    }
-
-    
-    private int findMinHammingDistance(ArrayList<int[]> listOfCodeWords) {
-        int minHamming = numOfBitsPerWord;
-        
-        for (int i = 0; i < numOfCodeWords; i++) {
-            for (int j = i+1; j < numOfCodeWords; j++) {
-                int currHamming = hammingDistance(listOfCodeWords.get(i), listOfCodeWords.get(j));
-                if (currHamming < minHamming) {
-                    minHamming = currHamming;
-                }
-            }
-        }
-        
-        return minHamming;
-    }
-    
+    }    
 
     private int hammingDistance(int[] codeWord1, int[] codeWord2){
         int count = 0;
