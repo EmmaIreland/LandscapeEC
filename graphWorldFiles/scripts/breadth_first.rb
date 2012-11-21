69e3f7543fca2592a9605fb05be8dde7c69ef2b1
irela065 <irela065@e5a9bd0e-96e4-4e5d-b9da-31dc9101860d>
2012-11-21 03:56:35 +0000

    worked on breadth first search
    git-svn-id: http://cscirepo.morris.umn.edu/svn/research/LandscapeEC@10874 e5a9bd0e-96e4-4e5d-b9da-31dc9101860d

--- graphWorldFiles/scripts/breadth_first.rb
+++ graphWorldFiles/scripts/breadth_first.rb
@@ -7,27 +7,31 @@ end
 yamlFile = ARGV[0]
 openFile = File.open( yamlFile )
 y = YAML::load_documents( openFile ) { |graph|
-	
+  
+  listOfAll = []
+  
 	for i in 0...graph.length-1
-	  queue = [i]
-    visited = {i => true}
-    print i.to_s() + " "
-    size = 1
-    
-    while(!queue.empty?)
-      node = queue.pop()
-      graph[node].each do |child|
+    if !listOfAll.include?(i)
+      queue = [i]
+      visited = {i => true}
+      print i.to_s() + " "
+      componentSize = 1
+      
+      while(!queue.empty?)
+        node = queue.pop()
+        graph[node].each do |child|
         
-				if visited[child] != true then
-				  print "#{child} "
-				  queue.push(child)
-				  visited[child] = true
-          size+=1
-				end
+				  if visited[child] != true then
+				    print "#{child} "
+            listOfAll.push(child)
+				    queue.push(child)
+				    visited[child] = true
+            componentSize += 1
+				  end
 				
-		  end
+		    end
+      end
+      puts "Component size: " + componentSize.to_s() + "\n "
 	  end
-	  
-	  puts "size: " + size.to_s()
 	end
 } 
