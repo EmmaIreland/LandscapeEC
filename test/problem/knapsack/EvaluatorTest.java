84bb52a381446adaeb34400ed636f94a823cca28
irela065 <irela065@e5a9bd0e-96e4-4e5d-b9da-31dc9101860d>
2012-12-28 02:38:23 +0000

    Wrote some junit tests for knapsack
    git-svn-id: http://cscirepo.morris.umn.edu/svn/research/LandscapeEC@10960 e5a9bd0e-96e4-4e5d-b9da-31dc9101860d

---
+++ test/problem/knapsack/EvaluatorTest.java
@@ -0,0 +1,58 @@
+package problem.knapsack;
+
+import static org.junit.Assert.*;
+import landscapeEC.problem.GlobalProblem;
+import landscapeEC.problem.Individual;
+import landscapeEC.problem.knapsack.KnapsackEvaluator;
+import landscapeEC.problem.knapsack.KnapsackProblem;
+
+import org.junit.Test;
+
+public class EvaluatorTest {
+
+    @Test
+    public void testKnapsack(){
+        KnapsackEvaluator evaluator = new KnapsackEvaluator();
+        GlobalProblem.setEvaluator(evaluator);
+        
+        int weightLimit = 10;
+        int[] values = {2, 4, 6, 8};
+        int[] weights = {1, 3, 7, 5};
+        KnapsackProblem problem = new KnapsackProblem(weightLimit, values, weights);
+        
+        GlobalProblem.setProblem(problem);
+        Individual individual = new Individual("1111");
+        assertEquals(14, evaluator.evaluate(problem, individual), 0);
+    }
+    
+    @Test
+    public void testKnapsack2(){
+        KnapsackEvaluator evaluator = new KnapsackEvaluator();
+        GlobalProblem.setEvaluator(evaluator);
+        
+        int weightLimit = 8;
+        int[] values = {2, 4, 6, 8};
+        int[] weights = {1, 9, 5, 13};
+        KnapsackProblem problem = new KnapsackProblem(weightLimit, values, weights);
+        
+        GlobalProblem.setProblem(problem);
+        Individual individual = new Individual("1010");
+        assertEquals(8, evaluator.evaluate(problem, individual), 0);
+    }
+    
+    @Test
+    public void testEmptyKnapsack(){
+        KnapsackEvaluator evaluator = new KnapsackEvaluator();
+        GlobalProblem.setEvaluator(evaluator);
+        
+        int weightLimit = 5;
+        int[] values = {2, 4, 6, 8};
+        int[] weights = {6, 7, 8, 9};
+        KnapsackProblem problem = new KnapsackProblem(weightLimit, values, weights);
+        
+        GlobalProblem.setProblem(problem);
+        Individual individual = new Individual("1111");
+        assertEquals(0, evaluator.evaluate(problem, individual), 0);
+    }
+    
+}
