f56ba1b9fb3d775cb21f631ce7c39e365ead7639
mcphee <mcphee@e5a9bd0e-96e4-4e5d-b9da-31dc9101860d>
2013-02-18 22:49:05 +0000

    We got the random walk eccentricity calculations to work in Python!
    git-svn-id: http://cscirepo.morris.umn.edu/svn/research/LandscapeEC@11905 e5a9bd0e-96e4-4e5d-b9da-31dc9101860d

--- graphWorldFiles/scripts/randomWalk.py
+++ graphWorldFiles/scripts/randomWalk.py
@@ -23,13 +23,32 @@ def readGraphFile(yamlFile):
 
 def computeTransitionProbabilities(paths):
   numNodes = paths.shape[0]
-  print(numNodes)
   degrees = sum(paths, axis=0)[0][0]
-  print(degrees)
   for i in range(numNodes):
     for j in range(numNodes):
       paths[i, j] = paths[i, j]/degrees[0, i]
   return paths
 
-print(computeTransitionProbabilities(readGraphFile(yamlFile)))
+def removeRowAndColumn(paths, index):
+  r = delete(paths,index, 0)
+  r = delete(r, index, 1)
+  return r
 
+def computeExpectedWalkTimeToNode(paths, node):
+  sub_matrix = removeRowAndColumn(paths, node)
+  print(node)
+  d = (identity(sub_matrix.shape[0], float) - sub_matrix).I
+  row_sums = d.sum(axis=1)
+  r = list(array(row_sums)[:, 0])
+  return r[0:node] + [0] + r[node:]
+
+paths = computeTransitionProbabilities(readGraphFile(yamlFile))
+
+distance_matrix = matrix([ computeExpectedWalkTimeToNode(paths, i) 
+			for i in range(0, paths.shape[0]) ])
+
+eccentricities = distance_matrix.max(axis=0)
+
+print(eccentricities)
+print(eccentricities.max())
+print(eccentricities.min())
